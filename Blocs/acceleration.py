63fcead432c9e7ca159bc03925caa6f0833b05ce
Adélie CASTEL <adelie.castel@viacesi.fr>
2024-04-06 21:13:53 +0200

    acceleration et position du joueur

--- Blocs/acceleration.py
+++ Blocs/acceleration.py
@@ -5,22 +5,25 @@ url = 'https://odyssey.haum.org/api/tokentest'
 class Vaisseau:
     def __init__(self, position):
         self.position = position
+        self.vitesse = [0, 0, 0]
 
     def accelerer(self, acceleration):
         # Ajoutez le vecteur d'accélération à la position actuelle du vaisseau
-        self.position = [self.position[i] + acceleration[i] for i in range(3)]
+        self.vitesse = [self.vitesse[0] + acceleration[0], self.vitesse[1] + acceleration[1], self.vitesse[2] + acceleration[2]]
+        self.position = [self.position[i] + self.vitesse[i] for i in range(3)]
+        # print(self.position, self.vitesse)
 
 
 # Exemple d'utilisation
 vaisseau = Vaisseau([0, 0, 0])
 
-vecteurs_acceleration = [[1, 0, 0],[1, 0, 0],[0, 1, 0]]  # Exemple de vecteurs d'accélération
+vecteurs_acceleration = [[1, 0, 0],[0, 1, 0]]  # Exemple de vecteurs d'accélération
 
 for acceleration in vecteurs_acceleration:
     vaisseau.accelerer(acceleration)
     print("Nouvelle position du vaisseau:", vaisseau.position)
 
-commandes = {}
+# commandes = {}
 
 
-response = requests.post(url, json=commandes)
+# response = requests.post(url, json=commandes)
