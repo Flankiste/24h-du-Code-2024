24fb05bf8a5ceaecb858f5b27a4d847a362fb97f
Killian HUBAULT <killian.hubault@gmail.com>
2024-04-06 23:46:28 +0200

    Création de l'accélération

--- mouvements.py
+++ mouvements.py
@@ -1,34 +1,39 @@
-def calcul_vitesse_chemin(liste): # détermine une liste de vitesses en fonction des points formant le chemin
+# /////////////////////////////// PARTIE FONCTIONS //////////////////////////////////
+
+def mouvement(liste):
     deplacements = []
-    for i in range(len(liste) - 1): # pour i parcourant la liste des points
+    deplacements.append([0, 0, 0])
+    for i in range(len(liste) - 1):
         deplacement = [0, 0, 0]
         for j in range(3):  # x, y, z
             diff = liste[i+1][j] - liste[i][j]
-            if diff == 1: # mouvement pour égaliser les positions
-                deplacement[j] = 1  # ajouter 1 à la coordonnée
-            elif diff == 0: # mouvement null
-                deplacement[j] = 0  # pas de déplacement
-        deplacements.append(deplacement) #ajoute le déplacement à la liste des déplacements
-    deplacements.insert(0, [0, 0, 0])  # Vitesse initiale
-    deplacements.append([0, 0, 0])  # Vitesse finale
+            if diff == 1:
+                deplacement[j] = 1
+            elif diff == 0:
+                deplacement[j] = 0
+        deplacements.append(deplacement)
+    deplacements.append([0, 0, 0])
     return deplacements
 
-def calcul_acceleration_chemin(vitesses: list[tuple[int, int, int]]): # détermine une liste d'accélérations en fonction des vitesses
+def rapide(liste2):
     accelerations = []
-    for i in range(0, len(vitesses) - 1): # pour i parcourant la liste des vitesses
-        vitesse_precedente = vitesses[i]
-        vitesse_suivante = vitesses[i + 1]
-        accelerations.append([vitesse_suivante[j] - vitesse_precedente[j] for j in range(3)]) # ajoute la différence des vitesses à la liste des accélérations
+    for i in range(len(liste2) - 1):
+        acceleration = [0, 0, 0]
+        for j in range(3):  # x, y, z
+            if liste2[i+1][j] == 1 and liste2[i][j] == 0:
+                acceleration[j] = 1
+            elif (liste2[i+1][j] == 1 and liste2[i][j] == 1) or (liste2[i+1][j] == 0 and liste2[i][j] == 0):
+                acceleration[j] = 0
+            else :
+                acceleration[j] = -1
+        accelerations.append(acceleration)
     return accelerations
-        
 
-# Test de la fonction avec chemin_test
+# /////////////////////////////// PARTIE TEST //////////////////////////////////
+
 chemin_test = [[0, 0, 0], [1, 0, 0], [2, 0, 0], [2, 1, 0], [2, 2, 0], [2, 2, 1], [2, 2, 2]]
-
-vitesses = calcul_vitesse_chemin(chemin_test)
-print("Vitesses:", vitesses)
-
-accelerations = calcul_acceleration_chemin(vitesses)
-print("Accélérations:", accelerations)
-
-# print("Mouvements nécessaires :", resultat)
+moove = mouvement(chemin_test)
+speed = rapide(moove)
+print("Chemin initial :", chemin_test)
+print("Mouvements nécessaires :", moove)
+print("Accéléraions nécessaires :", speed)
