cf784655d7e992f0b57974cd861a8a93411d474c
Arthur Nogues <arthur.nogues@etu.univ-nantes.fr>
2024-04-06 20:43:26 +0200

    Génération de carte finie - Envoie de la carte vers le serveur - Sauvegarde en txt - Génération de carte

--- map_creation.py
+++ map_creation.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 import requests
 import json
-
+from collections import defaultdict
 
 config = json.load(open("config.json"))
 
@@ -33,43 +33,68 @@ class Map:
         return quantity
     
     def generate(self):
+        """
+        Génère une map aléatoire
+        """
         print("Generating map...")
+        
+        # Définitions des quantités de blocs
         quantities = self.init_quantities()
         self.start = (random.randint(0, self.dimensions[0]-1), random.randint(0, self.dimensions[1]-1), random.randint(0, self.dimensions[2]-1))
-
         while sum(quantities.values()) < self.N//self.limit:
             quantities = self.init_quantities()
         
-        block_groups = {block_type: [] for block_type in quantities}
-        
+        # Placement des checkpoints
         for i in range(quantities["N_Checkpoint"]):
             x, y, z = (random.randint(0, self.dimensions[0]-1), random.randint(0, self.dimensions[1]-1), random.randint(0, self.dimensions[2]-1))
             cp = ['E//', 'F//', 'G//', 'H//']
             self.grid[x][y][z] = cp[i]
+        
+        # Placement des blocs de type A
+        for i in range(quantities["A//"]):
+            x, y, z = (random.randint(0, self.dimensions[0]-1), random.randint(0, self.dimensions[1]-1), random.randint(0, self.dimensions[2]-1))
+            self.grid[x][y][z] = 'A//'   
+        
+        
+        # Placements des centres des groupes de blocs
+        number_of_groups = random.randint(3, 20)
+        groups = defaultdict(dict)
+        for i in range(number_of_groups):
+            x, y, z = (random.randint(0, self.dimensions[0]-1), random.randint(0, self.dimensions[1]-1), random.randint(0, self.dimensions[2]-1))
+            if self.grid[x][y][z] == 'AAA':
+                block_types = ['B//', 'C//', 'D//']
+                block_type = block_types[random.randint(0, 2)]
+                groups[(x, y, z)] = {"type" : block_type, "size" : quantities[block_type]//number_of_groups}
+                self.grid[x][y][z] = block_type
+                
+        # Définit les décalages pour les 26 voisins dans une grille 3D
+        offsets = [(dx, dy, dz) for dx in (-1, 0, 1) for dy in (-1, 0, 1) for dz in (-1, 0, 1) if (dx, dy, dz) != (0, 0, 0)]
 
-        for block_type in quantities:
-            if block_type != "N_Checkpoint":
-                if quantities[block_type] > 0:
-                    while True:
-                        x, y, z = (random.randint(0, self.dimensions[0]-1), random.randint(0, self.dimensions[1]-1), random.randint(0, self.dimensions[2]-1))
-                        if self.grid[x][y][z] == 'AAA' and (x, y, z) != self.start:
-                            self.grid[x][y][z] = block_type
-                            block_groups[block_type].append((x, y, z))
-                            break
+        for center, properties in groups.items():
+            x, y, z = center
+            block_type = properties["type"]
+            size = properties["size"]
 
-                for _ in range(1, quantities[block_type]):
-                    while True:
-                        x, y, z = random.choice(block_groups[block_type])
-                        
-                        dx, dy, dz = random.choice([(0, 0, 1), (0, 0, -1), (0, 1, 0), (0, -1, 0), (1, 0, 0), (-1, 0, 0)])
+            # Commence une marche aléatoire à partir du centre du groupe
+            walk = [(x, y, z)]
 
-                        new_x, new_y, new_z = x + dx, y + dy, z + dz
+            # Marche aléatoire
+            for _ in range(size):
+                # Direction aléatoire
+                dx, dy, dz = random.choice(offsets)
 
-                        if 0 <= new_x < self.dimensions[0] and 0 <= new_y < self.dimensions[1] and 0 <= new_z < self.dimensions[2] and self.grid[new_x][new_y][new_z] == 'AAA':
-                            self.grid[new_x][new_y][new_z] = block_type
-                            block_groups[block_type].append((new_x, new_y, new_z))
-                            break
+                # Bouger dans la direction choisie
+                x, y, z = x + dx, y + dy, z + dz
 
+                # Vérifier si la nouvelle position est dans les limites de la grille
+                if 0 <= x < self.dimensions[0] and 0 <= y < self.dimensions[1] and 0 <= z < self.dimensions[2]:
+                    # ajouter la position à la liste
+                    walk.append((x, y, z))
+
+            # Remplir les blocs de la marche aléatoire
+            for x, y, z in walk:
+                self.grid[x][y][z] = block_type    
+        
         print("Map generated")
         
     def save(self):
@@ -96,8 +121,3 @@ class Map:
         response = requests.post(url, headers=headers, data=data)
         print(response.json())
         return response
-    
-essais = Map()
-
-essais.generate()
-essais.post()
